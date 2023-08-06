066b687c0b12b5cc31698b8cef4dd0b6ec3be215
Utkarsh Singh <utkarsh.singh9729@gmail.com>
2023-08-06 17:19:26 +0530

    itialCommit
    :wq

---
+++ Backend/scriptCom/scriptCom/src/main/java/com/scriptcom/core/scriptCom/ScriptComApplication.java
@@ -0,0 +1,13 @@
+package com.scriptcom.core.scriptCom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptComApplication.class, args);
+	}
+
+}
